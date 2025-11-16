b1b3eeeb05a766ac25c56b2be21fc693a2cc04ab
Aldeir Alves Martins <deirnogrc7@gmail.com>
2025-11-16 11:55:25 -0300

    teste

--- routes/api.php
+++ routes/api.php
@@ -156,3 +156,7 @@ Route::middleware('handler.exception')->group(function () {
         Route::apiResource('cart', CartController::class);
     });
 });
+Route::get('/produto/{id}', function ($id) {
+    $frontUrl = env('HOST_FRONT', 'https://commercefront.taskanalyzer.com');
+    return redirect()->away($frontUrl . '/product/' . $id);
+});
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/produto/{id}', function ($id) {
-    $frontUrl = env('HOST_FRONT', 'https://commercefront.taskanalyzer.com');
-    return redirect()->away($frontUrl . '/product/' . $id);
-});
