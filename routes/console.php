49e8d553c0f213fba57a5d7bdc27550122f3b43e
Aldeir Alves Martins <deirnogrc7@gmail.com>
2025-12-07 18:17:02 -0300

    teste

--- routes/console.php
+++ routes/console.php
@@ -11,19 +11,3 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Artisan::command('radio:cleanup', function () {
-    $cutoff = now()->subDays(90);
-    $sessionsCutoff = now()->subDays(7);
-
-    $deletedPlays = PlayedSong::query()->where('played_at', '<', $cutoff)->delete();
-    $deletedSessions = RadioSession::query()->where(function($q) use ($sessionsCutoff) {
-        $q->whereNull('last_saved_at')->orWhere('last_saved_at', '<', $sessionsCutoff);
-    })->delete();
-
-    Log::info('radio.cleanup', [
-        'deleted_plays' => $deletedPlays,
-        'deleted_sessions' => $deletedSessions,
-    ]);
-
-    $this->info("Cleanup done. played_songs deleted: {$deletedPlays}, radio_sessions deleted: {$deletedSessions}");
-})->purpose('Clean old radio sessions and played songs (TTL)');
